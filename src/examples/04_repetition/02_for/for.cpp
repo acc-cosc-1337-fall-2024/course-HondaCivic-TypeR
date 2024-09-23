f778e8275b6687b61cc649221ad621235eb1849d
HondaCivic-TypeR <francisco.rodriguez22@g.austincc.edu>
2024-09-23 23:44:16 +0000

    intro to for

--- src/examples/04_repetition/02_for/for.cpp
+++ src/examples/04_repetition/02_for/for.cpp
@@ -1,5 +1,14 @@
 #include "for.h"
-#include<iostream>
+
+using std::cout;
+
+void display_hello_for (int num)
+{
+    for(auto i = 0; i < num; i++) //auto i = 0 is the counter i++ is the incremental 1
+    {
+        cout<<"Hello\n";
+    }
+}
 
 //Write code for function display_numbers that accepts a number and 
 //displays every number that precedes(starting from 1) it in a line
