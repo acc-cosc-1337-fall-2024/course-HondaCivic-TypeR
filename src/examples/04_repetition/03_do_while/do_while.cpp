b36c0f0e048f2af6d79a9cf535a0c5f5af28c864
HondaCivic-TypeR <francisco.rodriguez22@g.austincc.edu>
2024-09-25 23:32:35 +0000

    sample do while menu

--- src/examples/04_repetition/03_do_while/do_while.cpp
+++ src/examples/04_repetition/03_do_while/do_while.cpp
@@ -18,4 +18,44 @@ void prompt_user()
 
 }
 
+void display_menu()
+{
+    cout<<"1 - Option 1"<<"\n";
+    cout<<"2 - Option 2"<<"\n";
+    cout<<"3 - Exit"<<"\n";
+}
 
+void run_menu()
+{
+    auto option = 0;
+
+    do
+    {
+        display_menu();
+        cout<<"Enter menu option: ";
+        cin>>option;
+
+        handle_user_option(option);
+
+    }while(option != 3); //requires semicolon
+}
+
+
+void handle_user_option(int num)
+{
+    switch(num)
+    {
+        case 1:
+            cout<<"You chose option 1.\n";
+            break;
+        case 2:
+            cout<<"You chose option 2.\n";
+            break;
+        case 3:
+            cout<<"Exiting....\n";
+            break;
+        default:
+            cout<<"Invalid Option.\n";
+            break;
+    }
+}
--- src/examples/04_repetition/03_do_while/do_while.h
+++ src/examples/04_repetition/03_do_while/do_while.h
@@ -3,6 +3,9 @@
 //Write void function protype named prompt_user with no parameters
 void prompt_user();
 
+void display_menu(); //assign functions one task
 
 //Write void function protype named run_menu with no parameters
+void run_menu();
 
+void handle_user_option(int num);
