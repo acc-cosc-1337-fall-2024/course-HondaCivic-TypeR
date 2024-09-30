dd74af5af13f3e0add747f4a95fec5416b5c52bb
HondaCivic-TypeR <francisco.rodriguez22@g.austincc.edu>
2024-09-30 23:43:47 +0000

    replace a string

--- src/examples/05_functions/01_void/main.cpp
+++ src/examples/05_functions/01_void/main.cpp
@@ -1,4 +1,13 @@
+#include"void.h"
+
+//if a else if t else if g else if c
+
+using std::string;
+
 int main()
 {
+    string lang = "Python";
+    replace_character_in_string(lang, 'P', 'p');
+    
     return 0;
 }
---
+++ src/examples/05_functions/01_void/void.h
@@ -0,0 +1,6 @@
+#include<string>
+#include<iostream>
+
+
+void display_string_characters(const std::string str);
+void replace_character_in_string(std::string str, char target, char replace_value);
