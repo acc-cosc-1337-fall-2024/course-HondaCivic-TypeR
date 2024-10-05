0dc31668ad3ea46ec1c07864d0885b8de7a6f8dd
HondaCivic-TypeR <francisco.rodriguez22@g.austincc.edu>
2024-10-05 03:05:45 +0000

    Rodriguez_Francisco_HOMEWORK_5

--- src/homework/05_functions/func.cpp
+++ src/homework/05_functions/func.cpp
@@ -1,3 +1,73 @@
 //add include statements
+#include "func.h"
 
 //add function code here
+double get_gc_content (const string& dna)
+{
+    double GCcount = 0;
+    double ATcount = 0;
+
+    for(auto i = 0; i < dna.size(); i++)
+    {
+        if(dna[i] == 'G')
+        {
+            GCcount += 1;
+        }
+        else if(dna[i] == 'C')
+        {
+            GCcount += 1;
+        }
+        else if(dna[i] == 'T')
+        {
+            ATcount += 1;
+        }
+        else if(dna[i] == 'A')
+        {
+            ATcount += 1;
+        }
+    }
+
+    double total = GCcount + ATcount;
+    double math = GCcount / total;
+
+    return math;
+}
+
+string reverse_string(string dna)
+{
+    string reverse = "";
+
+    for(int i = dna.size() - 1; i >= 0; i--) //have to use int | using auto makes i an 'unsigned long i' which causes core dump
+    {
+        reverse += dna[i];
+    }
+
+    return reverse;
+}
+
+string get_dna_complement(string dna)
+{
+    string reversed = reverse_string(dna);
+
+    for(auto i = 0; i < reversed.size(); i++)
+    {
+        if(reversed[i] == 'A')
+        {
+            reversed[i] = 'T';
+        }
+        else if(reversed[i] == 'T')
+        {
+            reversed[i] = 'A';
+        }
+        else if(reversed[i] == 'C')
+        {
+            reversed[i] = 'G';
+        }
+        else if(reversed[i] == 'G')
+        {
+            reversed[i] = 'C';
+        }
+    }
+
+    return reversed;
+}
--- src/homework/05_functions/main.cpp
+++ src/homework/05_functions/main.cpp
@@ -1,6 +1,57 @@
+#include "func.h"
+#include<iomanip>
+using std::cout; using std::cin; using std::setprecision;
 
 int main() 
 {
+	auto entry = 0;
+	string strand = "";
+
+	cout<<"Welcome to DNA Industries! \n";
+	cout<<"To check your DNA's GC content, press '1'.\n";
+	cout<<"To see your DNA's compliment, press '2'.\n";
+	cout<<"To exit, press '3'.\n";
+	cin>>entry;
+
+	do
+	{
+		if(entry == 1)
+		{
+			cout<<"Looks like you want to see your DNA's GC content. \n";
+			cout<<"To begin, please use the space provided below to input your strand of DNA: \n";
+			cin>>strand;
+
+			cout<<"The GC content of "<<strand<<" is: "<<setprecision(4)<<get_gc_content(strand)<<"\n\n";
+
+			cout<<"To check your DNA's GC content again, press '1'.\n";
+			cout<<"To see your DNA's compliment, press '2'.\n";
+			cout<<"To exit, press '3'.\n";
+			cin>>entry;
+		}
+		else if(entry == 2)
+		{
+			cout<<"Looks like you want to see your DNA's compliment. \n";
+			cout<<"To begin, use the space provided below to input your strand of DNA: \n";
+			cin>>strand;
+
+			cout<<"The compliment of "<<strand<<" is: "<<get_dna_complement(strand)<<"\n\n";
+
+			cout<<"To check your DNA's GC content, press '1'.\n";
+			cout<<"To see your DNA's compliment again, press '2'.\n";
+			cout<<"To exit, press '3'.\n";
+			cin>>entry;
+		}
+		else if(entry == 3)
+		{
+			cout<<"Exiting. Goodbye.\n";
+			break;
+		}
+		else
+		{
+			cout<<"Error.\n";
+		}
+		
+	} while (entry <= 3);
 
 	return 0;
 }
