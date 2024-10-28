622dd2f6d0f68626d8aa360c319c53277add15d2
HondaCivic-TypeR <francisco.rodriguez22@g.austincc.edu>
2024-10-28 04:22:42 +0000

    Rodriguez_Francisco_HOMEWORK_6

--- src/homework/06_tic_tac_toe/main.cpp
+++ src/homework/06_tic_tac_toe/main.cpp
@@ -1,6 +1,39 @@
+#include"tic_tac_toe.h"
 
-int main() 
+#include<iostream>
+#include<string>
+
+using std::cin; using std::cout;
+using std::string;
+
+
+int main()
 {
+    TicTacToe game;
+    string first_player;
+    char entry = 'y';
 
-	return 0;
+    do
+    {
+        cout<<"Please enter either 'X or 'O' for your first player: ";
+        cin>>first_player;
+
+        game.start_game(first_player);
+
+        int position;
+
+        while(!game.game_over())
+        {
+            cout<<"Choose a position on the board: ";
+            cin>>position;
+            game.mark_board(position);
+			game.display_board();
+        }
+
+        cout<<"Would you like to play again? \n";
+        cin>>entry;
+
+    }while(entry == 'Y' || entry == 'y');
+    
+    return 0;
 }
